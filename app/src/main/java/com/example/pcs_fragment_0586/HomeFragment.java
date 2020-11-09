908804b6050bed4477744f790a595cf0f9d5ac5b
steven73-hub <steven.27@students.amikom.ac.id>
2020-11-09 11:54:52 +0700

    fragment

---
+++ app/src/main/java/com/example/pcs_fragment_0586/HomeFragment.java
@@ -0,0 +1,79 @@
+package com.example.pcs_fragment_0586;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class HomeFragment extends Fragment implements View.OnClickListener{
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home2, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_category) {
+            CategoryFragment mCategoryFragment = new CategoryFragment();
+            FragmentManager mFragmentManager = getFragmentManager();
+            if (mFragmentManager != null) {
+                mFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_container, mCategoryFragment, CategoryFragment.class.getSimpleName())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        }
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnCategory = view.findViewById(R.id.btn_category);
+        btnCategory.setOnClickListener(this);
+
+    }
+}
